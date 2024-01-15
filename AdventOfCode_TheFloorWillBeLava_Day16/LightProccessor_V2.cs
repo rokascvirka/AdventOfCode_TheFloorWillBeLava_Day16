6c9d463853896b3930cf4207bb205906e2e88fa7
RokasCvirka <rokas42@gmail.com>
2024-01-15 13:44:55 +0200

    add fixed directions movement

--- AdventOfCode_TheFloorWillBeLava_Day16/LightProccessor_V2.cs
+++ AdventOfCode_TheFloorWillBeLava_Day16/LightProccessor_V2.cs
@@ -30,119 +30,139 @@ namespace AdventOfCode_TheFloorWillBeLava_Day16
         {
             
             Console.WriteLine();
-
             var coords = step.Symbol.Coordinates;
             if (coords.X.HasValue)
             {
                 var x = coords.X.Value;
                 var y = coords.Y.Value;
+                var ogSymbol = map.Map[x][y];
 
-                if (map.Map[x][y].Character == ".")
+                if (ogSymbol.Character == ".")
                 {
-                    if(step.DirectionLightCameFrom == DirectionEnum.MoveToRight)
+                    if(step.DirectionLightCameFrom == DirectionEnum.MoveToRight && !ogSymbol.IsLeftVisited)
                     {
                         MoveRight(map, coords, queue);
+                        ogSymbol.IsLeftVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToLeft)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToLeft && !ogSymbol.IsRightVisited)
                     {
                         MoveLeft(map, coords, queue);
+                        ogSymbol.IsRightVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveUp)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveUp && !ogSymbol.IsBottomVisited)
                     {
                         MoveUp(map, coords, queue);
+                        ogSymbol.IsBottomVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveDown)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveDown && !ogSymbol.IsTopVisited)
                     {
                         MoveDown(map, coords, queue);
+                        ogSymbol.IsTopVisited = true;
                     }
                 }
 
                 if (map.Map[x][y].Character == "\\")
                 {
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToRight)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToRight && !ogSymbol.IsLeftVisited)
                     {
                         MoveLeft(map, coords, queue);
                         MoveDown(map, coords, queue);
+                        ogSymbol.IsLeftVisited= true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToLeft)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToLeft && !ogSymbol.IsRightVisited)
                     {
                         MoveRight(map, coords, queue);
                         MoveUp(map, coords, queue);
+                        ogSymbol.IsRightVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveUp)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveUp && !ogSymbol.IsBottomVisited)
                     {
                         MoveLeft(map, coords, queue);
                         MoveDown(map, coords, queue);
+                        ogSymbol.IsBottomVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveDown)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveDown && !ogSymbol.IsTopVisited)
                     {
                         MoveRight(map, coords, queue);
                         MoveUp(map, coords, queue);
+                        ogSymbol.IsTopVisited = true;
                     }
                 }
                 if (map.Map[x][y].Character == "/")
                 {
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToRight)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToRight && !ogSymbol.IsLeftVisited)
                     {
                         MoveLeft(map, coords, queue);
                         MoveUp(map, coords, queue);
+                        ogSymbol.IsLeftVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToLeft)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToLeft && !ogSymbol.IsRightVisited)
                     {
                         MoveRight(map, coords, queue);
                         MoveDown(map, coords, queue);
+                        ogSymbol.IsRightVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveUp)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveUp && !ogSymbol.IsBottomVisited)
                     {
                         MoveRight(map, coords, queue);
                         MoveDown(map, coords, queue);
+                        ogSymbol.IsBottomVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveDown)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveDown && !ogSymbol.IsTopVisited)
                     {
                         MoveLeft(map, coords, queue);
                         MoveUp(map, coords, queue);
+                        ogSymbol.IsTopVisited = true;
                     }
                 }
                 if (map.Map[x][y].Character == "|")
                 {
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToRight)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToRight && !ogSymbol.IsLeftVisited)
                     {
                         MoveUp(map, coords, queue);
                         MoveDown(map, coords, queue);
+                        ogSymbol.IsLeftVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToLeft)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToLeft && !ogSymbol.IsRightVisited)
                     {
                         MoveUp(map, coords, queue);
                         MoveDown(map, coords, queue);
+                        ogSymbol.IsRightVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveUp)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveUp && !ogSymbol.IsBottomVisited)
                     {
                         MoveUp(map, coords, queue);
+                        ogSymbol.IsBottomVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveDown)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveDown && !ogSymbol.IsTopVisited)
                     {
                         MoveDown(map, coords, queue);
+                        ogSymbol.IsTopVisited = true;
                     }
                 }
                 if (map.Map[x][y].Character == "-")
                 {
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToRight)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToRight && !ogSymbol.IsLeftVisited)
                     {
                         MoveRight(map, coords, queue);
+                        ogSymbol.IsLeftVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToLeft)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveToLeft && !ogSymbol.IsRightVisited)
                     {
                         MoveRight(map, coords, queue);
+                        ogSymbol.IsRightVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveUp)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveUp && !ogSymbol.IsBottomVisited)
                     {
                         MoveRight(map, coords, queue);
                         MoveLeft(map, coords, queue);
+                        ogSymbol.IsBottomVisited = true;
                     }
-                    if (step.DirectionLightCameFrom == DirectionEnum.MoveDown)
+                    if (step.DirectionLightCameFrom == DirectionEnum.MoveDown && !ogSymbol.IsTopVisited)
                     {
                         MoveRight(map, coords, queue);
                         MoveLeft(map, coords, queue);
+                        ogSymbol.IsTopVisited = true;
                     }
                 }
             }
@@ -150,7 +170,7 @@ namespace AdventOfCode_TheFloorWillBeLava_Day16
 
         private static void MoveRight(LightMapModel map, Coordinates startCooridnates, Queue<QueueModel> queue)
         {
-            var SymbolFoundCoordinates = new Coordinates();
+            var symbolFoundCoordinates = new Coordinates();
             var startCoordsX = startCooridnates.X.Value;
             var startCoordsY = startCooridnates.Y.Value;
             var startSymbol = map.Map[startCoordsX][startCoordsY];
@@ -166,9 +186,9 @@ namespace AdventOfCode_TheFloorWillBeLava_Day16
 
                 if (SPECIAL_CHARACTERS.Any(x => x.Equals(currentSymbol.Character.ToString())))
                 {
-                    SymbolFoundCoordinates.X = currentSymbol.Coordinates.X;
-                    SymbolFoundCoordinates.Y = currentSymbol.Coordinates.Y;
-                    UpdateSymbolFoundToHashTag(map, SymbolFoundCoordinates.X.Value, SymbolFoundCoordinates.Y.Value);
+                    symbolFoundCoordinates.X = currentSymbol.Coordinates.X;
+                    symbolFoundCoordinates.Y = currentSymbol.Coordinates.Y;
+                    UpdateSymbolFoundToHashTag(map, symbolFoundCoordinates.X.Value, symbolFoundCoordinates.Y.Value);
                     PrintCurrentUpdatedMap(map);
                     break;
                 }
@@ -176,51 +196,40 @@ namespace AdventOfCode_TheFloorWillBeLava_Day16
                 UpdateToHashTag(map, startCoordsX, column);
                 PrintCurrentUpdatedMap(map);
             }
-            var symbolFound = map.Map[SymbolFoundCoordinates.X.Value][SymbolFoundCoordinates.Y.Value];
-
-            if (symbolFound != null)
-            {
-                queue.Enqueue(new QueueModel(symbolFound, DirectionEnum.MoveToRight));
-            }
-
+            AddToQueue(map, symbolFoundCoordinates.X, symbolFoundCoordinates.Y, queue, DirectionEnum.MoveToRight);
         }
         private static void MoveLeft(LightMapModel map, Coordinates startCooridnates, Queue<QueueModel> queue)
         {
-            var SymbolFoundCoordinates = new Coordinates();
+            var symbolFoundCoordinates = new Coordinates();
             var startCoordsX = startCooridnates.X.Value;
             var startCoordsY = startCooridnates.Y.Value;
             var startSymbol = map.Map[startCoordsX][startCoordsY];
             
-            for(int column = startCoordsY; column >= 0; column--)
+            for(int column = startCoordsY-1; column >= 0; column--)
             {
                 var currentSymbol = map.Map[startCoordsX][column];
                 if (SPECIAL_CHARACTERS.Any(x => x.Equals(currentSymbol.Character.ToString())))
                 {
-                    SymbolFoundCoordinates.X = currentSymbol.Coordinates.X;
-                    SymbolFoundCoordinates.Y = currentSymbol.Coordinates.Y;
-                    UpdateSymbolFoundToHashTag(map, SymbolFoundCoordinates.X.Value, SymbolFoundCoordinates.Y.Value);
+                    symbolFoundCoordinates.X = currentSymbol.Coordinates.X;
+                    symbolFoundCoordinates.Y = currentSymbol.Coordinates.Y;
+                    UpdateSymbolFoundToHashTag(map, symbolFoundCoordinates.X.Value, symbolFoundCoordinates.Y.Value);
                     PrintCurrentUpdatedMap(map);
                     break;
                 }
                 UpdateToHashTag(map, startCoordsX, column);
                 PrintCurrentUpdatedMap(map);
             }
-            var symbolFound = map.Map[SymbolFoundCoordinates.X.Value][SymbolFoundCoordinates.Y.Value];
-
-            if (symbolFound != null)
-            {
-                queue.Enqueue(new QueueModel(symbolFound, DirectionEnum.MoveToLeft));
-            }
+            AddToQueue(map, symbolFoundCoordinates.X, symbolFoundCoordinates.Y, queue, DirectionEnum.MoveToLeft);
         }
         private static void MoveDown(LightMapModel map, Coordinates startCooridnates, Queue<QueueModel> queue)
         {
-            var SymbolFoundCoordinates = new Coordinates();
+            var symbolFoundCoordinates = new Coordinates();
             var startCoordsX = startCooridnates.X.Value;
             var startCoordsY = startCooridnates.Y.Value;
             var startSymbol = map.Map[startCoordsX][startCoordsY];
             var rowsCount = map.Map.Count;
 
-            if(startCoordsX >= 0 && startCoordsX < rowsCount)
+            if (startCoordsX >= 0 && startCoordsX < rowsCount)
             {
                 for(int row = startCoordsX + 1; row < rowsCount; row++)
                 {
@@ -228,29 +237,23 @@ namespace AdventOfCode_TheFloorWillBeLava_Day16
 
                     if (SPECIAL_CHARACTERS.Any(x => x.Equals(currentSymbol.Character.ToString())))
                     {
-                        SymbolFoundCoordinates.X = currentSymbol.Coordinates.X;
-                        SymbolFoundCoordinates.Y = currentSymbol.Coordinates.Y;
-                        UpdateSymbolFoundToHashTag(map, SymbolFoundCoordinates.X.Value, SymbolFoundCoordinates.Y.Value);
+                        symbolFoundCoordinates.X = currentSymbol.Coordinates.X;
+                        symbolFoundCoordinates.Y = currentSymbol.Coordinates.Y;
+                        UpdateSymbolFoundToHashTag(map, symbolFoundCoordinates.X.Value, symbolFoundCoordinates.Y.Value);
                         PrintCurrentUpdatedMap(map);
                         break;
                     }
+                    
                     UpdateSymbolFoundToHashTag(map, row, startCoordsY);
                     PrintCurrentUpdatedMap(map);
                 }
             }
 
-            if (SymbolFoundCoordinates.X != null && SymbolFoundCoordinates.Y != null)
-            {
-                var symbolFound = map.Map[SymbolFoundCoordinates.X.Value][SymbolFoundCoordinates.Y.Value];
-                if (symbolFound != null)
-                {
-                    queue.Enqueue(new QueueModel(symbolFound, DirectionEnum.MoveDown));
-                }
-            }
+            AddToQueue(map, symbolFoundCoordinates.X, symbolFoundCoordinates.Y, queue, DirectionEnum.MoveDown);
         }
         private static void MoveUp(LightMapModel map, Coordinates startCooridnates, Queue<QueueModel> queue)
         {
-            var SymbolFoundCoordinates = new Coordinates();
+            var symbolFoundCoordinates = new Coordinates();
             var startCoordsX = startCooridnates.X.Value;
             var startCoordsY = startCooridnates.Y.Value;
             var startSymbol = map.Map[startCoordsX][startCoordsY];
@@ -259,14 +262,14 @@ namespace AdventOfCode_TheFloorWillBeLava_Day16
 
             if(startCoordsX <= rowsCount && startCoordsX > 0)
             {
-                for(int row =  startCoordsX - 1; row > 0; row--)
+                for(int row =  startCoordsX - 1; row >= 0; row--)
                 {
                     var currentSymbol = map.Map[row][startCoordsY];
                     if (SPECIAL_CHARACTERS.Any(x => x.Equals(currentSymbol.Character.ToString())))
                     {
-                        SymbolFoundCoordinates.X = currentSymbol.Coordinates.X;
-                        SymbolFoundCoordinates.Y = currentSymbol.Coordinates.Y;
-                        UpdateSymbolFoundToHashTag(map, SymbolFoundCoordinates.X.Value, SymbolFoundCoordinates.Y.Value);
+                        symbolFoundCoordinates.X = currentSymbol.Coordinates.X;
+                        symbolFoundCoordinates.Y = currentSymbol.Coordinates.Y;
+                        UpdateSymbolFoundToHashTag(map, symbolFoundCoordinates.X.Value, symbolFoundCoordinates.Y.Value);
                         PrintCurrentUpdatedMap(map);
                         break;
                     }
@@ -274,14 +277,7 @@ namespace AdventOfCode_TheFloorWillBeLava_Day16
                     PrintCurrentUpdatedMap(map);
                 }
             }
-            if (SymbolFoundCoordinates.X != null && SymbolFoundCoordinates.Y != null)
-            {
-                var symbolFound = map.Map[SymbolFoundCoordinates.X.Value][SymbolFoundCoordinates.Y.Value];
-                if (symbolFound != null)
-                {
-                    queue.Enqueue(new QueueModel(symbolFound, DirectionEnum.MoveUp));
-                }
-            }
+            AddToQueue(map, symbolFoundCoordinates.X, symbolFoundCoordinates.Y, queue, DirectionEnum.MoveUp);
         }
 
 
@@ -342,6 +338,18 @@ namespace AdventOfCode_TheFloorWillBeLava_Day16
         private static void UpdateToHashTag(LightMapModel map, int x, int y)
         {
             map.UpdatedMap[x][y].Character = "#";
+        }
+
+        private static void AddToQueue(LightMapModel map, int? symbolFoundCoordinateX, int? symbolFoundCoordinateY, Queue<QueueModel> queue, DirectionEnum direction)
+        {
+            if (symbolFoundCoordinateX != null && symbolFoundCoordinateY != null)
+            {
+                var symbolFound = map.Map[symbolFoundCoordinateX.Value][symbolFoundCoordinateY.Value];
+                if (symbolFound != null)
+                {
+                    queue.Enqueue(new QueueModel(symbolFound, direction));
+                }
+            }
         }
     }
 }
